01fdae841feda51acb43020825ac686382e1b5de
Ellise P. <ellise.pagong@gmail.com>
2025-07-10 16:52:59 +0800

    refactor index.html

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,13 +1,27 @@
+function showElement(elem) {
+  elem.style.display = "block"; 
+  void elem.offsetWidth;
+  elem.classList.remove("hidden");
+  elem.classList.add("visible");
+}
+
+function hideElement(elem) {
+  elem.classList.remove("visible");
+  elem.classList.add("hidden");
+  elem.addEventListener("transitionend", function handler() {
+    elem.style.display = "none";
+    elem.removeEventListener("transitionend", handler);
+  });
+}
 
 document.getElementById("to-register").addEventListener("click", () => {
-  document.getElementById("login-card").classList.add("hidden");
-  document.getElementById("register-card").classList.remove("hidden")
-  document.getElementById("register-card").classList.add("container-register");
+  hideElement(document.getElementById("login-card"));
+  showElement(document.getElementById("register-card"));
 });
 
 document.getElementById("to-login").addEventListener("click", () => {
-  document.getElementById("login-card").classList.remove("hidden");
-  document.getElementById("register-card").classList.add("hidden")
+  hideElement(document.getElementById("register-card"));
+  showElement(document.getElementById("login-card"));
 });
 
 document.getElementById("login").addEventListener("click", ()=>{
