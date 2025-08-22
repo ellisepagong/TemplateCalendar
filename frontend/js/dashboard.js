b2532a1e8ee1c5f3aa2b85ce805f5addcdbea3da
ellisepagong <ellise.pagong@gmail.com>
2025-08-22 16:06:42 +0800

    Moved population functions to population.js

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -3,3 +3,52 @@
     Global Listener and Initial population
 
 */
+
+// IMPORTS
+import {generateCalendar, populateSavedTasks, populateSavedTemplates, populateDayView} from "./population.js";
+
+
+// INITIAL POPULATION
+
+// calendar
+const today = new Date();
+generateCalendar(today.getMonth(), today.getFullYear(), today.getDate());
+// saved tasks and templates
+populateSavedTasks();
+populateSavedTemplates();
+// populate day view
+populateDayView(today);
+
+
+
+
+// GLOBAL LISTENER
+document.addEventListener("click", function (event) {
+    // format:
+    //
+    //     if (event.target.matches(/*CLASS OR ID IN QUOTES*/)) {
+    //         // if dynamic element
+    //
+    //         const ID = event.target.dataset./*ID FORMAT*/;
+    //         const template = document.getElementById(/*id format ${ID}*/);
+    //        
+    //         //  logic here
+    //     }
+    //
+    
+
+    // Sidebar Buttons
+
+
+
+    // Day View / Agenda
+
+
+    
+    // Saved Items View
+
+
+
+    // Calendar View
+
+});
